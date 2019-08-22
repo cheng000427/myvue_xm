ec91645492f78fb20ba3d0068498f645c62061d2
郭城 <1501093240@qq.com>
2019-08-22 16:15:32 +0800

    实现登录页面的静态页面

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@/styles/index.less'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router/router.js'
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
